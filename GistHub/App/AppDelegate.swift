591cc1536cd2ba2db0a31cd5552345e83e6f1d16
ldakhoa <ldakhoa1308@gmail.com>
2022-12-10 03:26:32 +0700

    Init project

---
+++ GistHub/App/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  GistHub
+//
+//  Created by Khoa Le on 10/12/2022.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+   
+        return true
+    }
+
+    /// Bootstrap a new window with root view controller to display.
+    private func boostrapWidow() {
+        if #available(iOS 13, *) {
+            // The `SceneDelegate` take reponsibilities for bootstrapping the window.
+        } else {
+            let window = UIWindow()
+            self.window = window
+            let viewController = UIViewController()
+            window.rootViewController = viewController
+            window.makeKeyAndVisible()
+        }
+    }
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {}
+}
---
+++ GistHub/App/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  GistHub
+//
+//  Created by Khoa Le on 10/12/2022.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        boostrapWidow(from: scene)
+    }
+    
+    /// Bootstrap a new window with root view controller to display.
+    private func boostrapWidow(from scene: UIScene) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        // Make a window and then save it for later usuage.
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        // Make initial view controller.
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        let navigationController = UINavigationController(rootViewController: viewController)
+        // Display the initial view controller.
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        // Share the window to the `AppDelegate`.
+        (UIApplication.shared.delegate as? AppDelegate)?.window = window
+    }
+}
