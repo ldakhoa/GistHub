e8c41261c3b34d76505dfa7aacc0ab1afb2f0329
ldakhoa <ldakhoa1308@gmail.com>
2022-12-19 13:58:37 +0700

    Use secret env in Xcode build step

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -2,8 +2,6 @@
 
 echo "Stage: PRE-Xcode Build is activated ..."
 
-export $(cat .env | grep -v '#' | awk '/=/ {print $0} {print $1}' )
-
 cd GistHub/Resources
 
 plutil -replace GITHUBID -string $GITHUBID Info.plist
