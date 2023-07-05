d09a919bf7fd894288c56c23bcf666f56f88a291
Khoa Le <ldakhoa1308@gmail.com>
2023-07-05 20:15:48 +0700

    Fix typo

--- GistHub/Components/GistDetail/GistDetailView.swift
+++ GistHub/Components/GistDetail/GistDetailView.swift
@@ -149,7 +149,7 @@ struct GistDetailView: View {
             ToolbarItem(placement: .navigationBarTrailing) {
                 Menu {
                     if userStore.user.id == gist.owner?.id {
-                        makeMenuButton(title: "Edit gist", systemImage: "pencil") {
+                        makeMenuButton(title: "Edit Gist", systemImage: "pencil") {
                             showEditGist.toggle()
                         }
                         makeMenuButton(title: "Edit Description", systemImage: "pencil") {
