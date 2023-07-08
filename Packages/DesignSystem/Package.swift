7723e059a97666714ce90aaf973828cb07118854
Khoa Le <ldakhoa1308@gmail.com>
2023-07-09 00:35:27 +0700

    Fix build error on CI

--- Packages/DesignSystem/Package.swift
+++ Packages/DesignSystem/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/elai950/AlertToast", from: "1.3.9"),
-        .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: "7.0.0"))
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.8.1")
     ],
     targets: [
         .target(
