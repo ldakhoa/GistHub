72c718e00448955e0d2122b6a9b9000dd4405fa5
Khoa Le <ldakhoa1308@gmail.com>
2023-07-14 23:07:29 +0700

    Format scripting file

--- Scripts/resolve_packages.sh
+++ Scripts/resolve_packages.sh
@@ -13,15 +13,14 @@ packages_dir="$source_root/Packages"
 # Start in the packages directory
 cd "$packages_dir"
 
-GREEN='\033[0;32m'  # Green
-NC='\033[0m'  # No Color
+GREEN='\033[0;32m' # Green
+NC='\033[0m'       # No Color
 
-for item in */
-do
+for item in */; do
 	# Check if the item is a directory
 	if [[ -d "$item" ]]; then
 		# Remove trailing slashes to get the directory name only
-    dir_name=${item%%/}
+		dir_name=${item%%/}
 		# Enter the directory and resolve the package
 		cd "$item"
 		printf "${GREEN}Resolving $dir_name...${NC}\n"
