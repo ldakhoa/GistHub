692f3a6d084f3074a78a78e6462efd5d858043b7
Khoa Le <ldakhoa1308@gmail.com>
2023-07-14 23:14:42 +0700

    Move every scripts to `Scripts/` and rewrite Package Generator from Swift to bash

---
+++ Scripts/generate_package.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Args
+if [ "$#" -lt 1 ]; then
+    echo "Please provide a package name."
+    exit 0
+fi
+
+packageName=$1
+
+echo "🔨 Starting to generate $packageName..."
+
+rootDir=$(pwd)
+packageDir="$rootDir/Packages/$packageName"
+
+mkdir -p "$packageDir"
+
+packageContents=$(cat <<EOF
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "$packageName",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "$packageName",
+            targets: ["$packageName"]
+        ),
+    ],
+    dependencies: [
+        // Local package dependencies
+        // .package(name: "Package name", path: "../Package name"),
+        // Third package dependencies
+        // .package(url: "url", from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "$packageName",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "${packageName}Tests",
+            dependencies: ["$packageName"]
+        ),
+    ]
+)
+EOF
+)
+
+gitIgnoreContents=$(cat <<EOF
+.DS_Store
+/.build
+/Packages
+xcuserdata/
+DerivedData/
+.netrc
+.swiftpm/
+EOF
+)
+
+echo "$packageContents" > "$packageDir/Package.swift"
+echo "$gitIgnoreContents" > "$packageDir/.gitignore"
+
+sourceDir="$packageDir/Sources/$packageName"
+testDir="$packageDir/Tests/${packageName}Tests"
+mkdir -p "$sourceDir"
+mkdir -p "$testDir"
+
+touch "$sourceDir/$packageName.swift"
+touch "$testDir/${packageName}Tests.swift"
+
+echo "🔄 Resolving package..."
+# Change directory and resolve package
+cd "$packageDir" && swift package resolve
+
+if [ $? -eq 0 ]; then
+    echo "️✅ Generated $packageName."
+else
+    echo "❌ Failed to resolve package."
+fi
