c2fe3758f62f023a57f808ac2ea6a92c171b0db1
Khoa Le <ldakhoa1308@gmail.com>
2023-07-28 12:49:20 +0700

    Fetch the current user in ProfileView

--- Packages/Profile/Sources/Profile/ProfileView.swift
+++ Packages/Profile/Sources/Profile/ProfileView.swift
@@ -20,52 +20,9 @@ public struct ProfileView: View {
     @EnvironmentObject private var appAccountsManager: AppAccountsManager
     @EnvironmentObject private var routerPath: RouterPath
 
-    @StateObject private var viewModel = ProfileViewModel()
-
     public init() {}
 
     public var body: some View {
-        //        ZStack {
-        //            if let user = currentAccount.user {
-        //                makeMainView(user: user)
-        //            } else {
-        //                switch viewModel.contentState {
-        //                case .loading:
-        //                    makeMainView(user: .stubbed)
-        //                        .redacted(reason: .placeholder)
-        //                case let .content(user):
-        //                    makeMainView(user: user)
-        //                case let .error(error):
-        //                    Text(error)
-        //                        .foregroundColor(Colors.danger.color)
-        //                }
-        //            }
-        //        }
-        UserProfileView(userName: "ldakhoa")
-        //        .navigationBarTitleDisplayMode(.inline)
-        //        .toolbar {
-        //            ToolbarItem(placement: .navigationBarTrailing) {
-        //                HStack {
-        //                    Button {
-        //                        routerPath.navigate(to: .settings)
-        //                    } label: {
-        //                        Image(systemName: "gear")
-        //                            .foregroundColor(Colors.accent.color)
-        //                    }
-        //
-        //                    let titlePreview = "\(currentAccount.user?.login ?? "") - Overview"
-        //                    ShareLink(
-        //                        item: currentAccount.user?.htmlURL ?? "",
-        //                        preview: SharePreview(titlePreview, image: Image("default"))
-        //                    ) {
-        //                        Image(systemName: "square.and.arrow.up")
-        //                            .foregroundColor(Colors.accent.color)
-        //                    }
-        //                }
-        //            }
-        //        }
-        //        .toolbarBackground(UIColor.systemGroupedBackground.color, for: .navigationBar)
-        //        .refreshable { fetchUser() }
-        //        .enableInjection()
+        UserProfileView(userName: currentAccount.user?.login ?? "")
     }
 }
