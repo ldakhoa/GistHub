959fee5fb7aa0d649720b399eb94895cc5d78957
Khoa Le <ldakhoa1308@gmail.com>
2023-08-04 22:28:23 +0700

    Fix tab bar bug SwiftUI

--- GistHub/App/GistHubApp.swift
+++ GistHub/App/GistHubApp.swift
@@ -72,6 +72,16 @@ struct GistHubApp: App {
                     .tag(tab)
             }
         }
+        .onAppear {
+            // correct the transparency bug for Tab bars
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.configureWithOpaqueBackground()
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+            // correct the transparency bug for Navigation bars
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.configureWithOpaqueBackground()
+            UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        }
         .background(UIColor.systemBackground.color)
         .tint(Colors.accent.color)
     }
