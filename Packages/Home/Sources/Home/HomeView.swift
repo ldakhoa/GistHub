3f6cbace3514083b3720ea79c4a3dd9b339ce3d3
Khoa Le <ldakhoa1308@gmail.com>
2023-08-16 03:21:11 +0700

    Add Recent Section title

--- Packages/Home/Sources/Home/HomeView.swift
+++ Packages/Home/Sources/Home/HomeView.swift
@@ -42,36 +42,48 @@ public struct HomeView: View {
             }
 
             Section {
-                VStack(spacing: 12) {
-                    Text("Add favorite gists here for quick access anytime, without the need to search")
-                        .multilineTextAlignment(.center)
-
-                    Button(action: {
-                    }, label: {
-                        HStack {
-                            Spacer()
-                            Text("Add Quick Access")
-                                .font(.callout)
-                                .fontWeight(.semibold)
-                            Spacer()
-                        }
-                        .padding(12)
-                        .foregroundColor(Colors.accent.color)
-                        .cornerRadius(8)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Colors.buttonBorder.color)
-                        )
-                    })
-                }
-                .frame(maxWidth: .infinity)
+                quickAccessSection
             } header: {
                 Text("Quick Access")
+                    .headerProminence(.increased)
+            }
+
+            Section {
+                Text("Test Test Test")
+            } header: {
+                Text("Recent Activities")
                     .headerProminence(.increased)
             }
         }
         .navigationTitle("Home")
         .navigationBarTitleDisplayMode(.large)
+    }
+
+    @ViewBuilder
+    private var quickAccessSection: some View {
+        VStack(spacing: 12) {
+            Text("Add favorite gists here for quick access anytime, without the need to search")
+                .multilineTextAlignment(.center)
+
+            Button(action: {
+            }, label: {
+                HStack {
+                    Spacer()
+                    Text("Add Quick Access")
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                    Spacer()
+                }
+                .padding(12)
+                .foregroundColor(Colors.accent.color)
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Colors.buttonBorder.color)
+                )
+            })
+        }
+        .frame(maxWidth: .infinity)
     }
 
     @ViewBuilder
